2ca514b11d22eb2c0cf46992e5afbcd30c59e803
kathrynsujai <kss5985@psu.edu>
2026-04-26 18:29:24 -0400

    Update final_code.py
    Updates the code to reflect difficulties, fixing bugs and addressing errors.

--- final_code.py
+++ final_code.py
@@ -3,32 +3,50 @@
 #import the random method
 import random
 rand_num = random.randint(1,100)
+
 print("Welcome to the Number Guessing Game! You will have 7 attempts to guess a number from 1 to 100.")
-attempt_lim = 7
+print("There are three levels of diificulty, select 'easy', 'medium', 'hard'.")
+
+d = {'easy': 10, 'medium': 7,'hard': 5}
+choice = input("Choose difficulty: ")
+
+if choice not in d:
+    print("Not a valid choice, please select 'easy', 'medium', 'hard'")
+    choice = input("Choose difficulty: ")
+    if choice not in d:
+        print("Not a valid choice, defaulting to medium")
+        choice = 'medium'
+
+
+attempt_lim = d[choice]
+
+print(f"You will have {attempt_lim} attempts to guess a number from 1 to 100.")
 attempt = 0
 status = False
 
 while attempt < attempt_lim:
     guess = input("Enter your guess: ")
-    if guess.isdigit():
+
+    try:
         player_guess = int(guess)
-    else:
+    except ValueError:
         print("Please enter a number.")
-        guess = input("Enter your guess: ")
+        player_guess = None
 
-    attempt += 1
+    if player_guess is not None:
+        attempt += 1
     
-    if player_guess > rand_num:
-        print("Too high!")
-    elif player_guess < rand_num:
-        print("Too low!")
-    else:
-        print(f"Correct! You guessed it in {attempt} attempts!")
-        status = True
-        attempt = attempt_lim
+        if player_guess > rand_num:
+            print(f"Too high! You have {attempt_lim - attempt} attempts left")
+        elif player_guess < rand_num:
+            print(f"Too low! You have {attempt_lim - attempt} attempts left")
+        else:
+            print(f"Correct! You guessed it in {attempt} attempts!")
+            status = True
+            attempt = attempt_lim
 
-if not status:
-    print(f"Sorry, you're out of attempts. The number was {rand_num}.")
+    if not status:
+        print(f"Sorry, you're out of attempts. The number was {rand_num}.")
     
         
 
